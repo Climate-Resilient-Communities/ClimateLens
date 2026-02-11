10f80a38c2909367bc292e8bc3c6dccd9e39bbc0
Karim M. El-Sharkawy <karimelsharkawy2002@gmail.com>
2026-02-11 17:56:30 -0500

    added some util files

--- src/utils/sample_twitter.py
+++ src/utils/sample_twitter.py
@@ -4,12 +4,12 @@ import os
 from pathlib import Path
 import pandas as pd
 
-first_chunk_path = "..."
+first_chunk_path = ""
 df_first_chunk = pd.read_csv(first_chunk_path)
 
 df_sample = df_first_chunk.head(100_000)
 
-sample_path = "..."
+sample_path = "./twitter_sample.csv"
 df_sample.to_csv(sample_path, index=False)
 
 print(f"Sample dataset created: {sample_path}")
