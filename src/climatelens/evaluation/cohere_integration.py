c52273f3b021f91d66c2593a39356eb6822b455e
Karim M. El-Sharkawy <karimelsharkawy2002@gmail.com>
2026-05-05 14:05:34 -0400

    added function parameters and type annotations

--- src/climatelens/evaluation/cohere_integration.py
+++ src/climatelens/evaluation/cohere_integration.py
@@ -1,29 +1,33 @@
-### this file is used within the create_submodels() function in topic_modeling.py
+### this file is used within create_submodels() in topic_modeling.py
 
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+cohere_api_key = os.getenv("COHERE_API_KEY")
+
+from typing import List, Optional, Union
 
 import cohere
 from bertopic.representation import Cohere, MaximalMarginalRelevance
 
-# need to do env handeling later
-
 mmr_model = MaximalMarginalRelevance(diversity=0.3)
 
 
-def cohere_integration():
-    cohere_api_key = os.getenv("COHERE_API_KEY")
+def cohere_integration(KEYWORDS: List[str], DOCUMENTS: List[str]) -> Optional[Cohere]:
     if not cohere_api_key:
         print("No COHERE_API_KEY found in .env file, skipping Cohere representation.")
         return None
 
     try:
         cohere_client = cohere.Client(cohere_api_key)
-        custom_prompt = """
+        custom_prompt = f"""
         I have a topic described by the following keywords:
-        [KEYWORDS]
+        {KEYWORDS}
 
         The most representative documents for this topic are:
-        [DOCUMENTS]
+        {DOCUMENTS}
 
         Based on the information above, create a short topic label.
         Use 2-5 words maximum, no punctuation.
@@ -45,7 +49,7 @@ def cohere_integration():
 
 cohere_model = cohere_integration()
 if cohere_model:
-    representation_model = [mmr_model, cohere_model]
+    representation_model: List[Union[MaximalMarginalRelevance, Cohere]] = [mmr_model, cohere_model]
     print("Using MMR + Cohere for representation")
 else:
     representation_model = mmr_model
